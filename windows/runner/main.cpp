fe219b11168febf545c6a9a260e42dcf8356c969
chenyouhui1 <chenyouhui1@joyy.com>
2026-03-21 23:22:53 +0800

    Windows分辨率

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,8 +25,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(720, 1280);
+  Win32Window::Point origin(200, 0);
+  Win32Window::Size size(720, 960);
   if (!window.Create(L"beecount", origin, size)) {
     return EXIT_FAILURE;
   }
